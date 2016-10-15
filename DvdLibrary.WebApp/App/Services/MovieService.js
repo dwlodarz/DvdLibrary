e4727ed92be9568a919154092c9e77851027edbb
Dominik <dominik.wlodarz@gmail.com>
2016-10-15 21:20:13 +0200

    A little bit of cleaning up

--- DvdLibrary.WebApp/App/Services/MovieService.js
+++ DvdLibrary.WebApp/App/Services/MovieService.js
@@ -15,50 +15,8 @@
                 });
                 return getMoviesRequest;
             }
-
-            //function addNewEvent(event) {
-            //    var addEventRequest = $http({
-            //        method: "POST",
-            //        url: apiUri + 'event?eventGuid=' + event.guid,
-            //        dataType: 'json',
-            //        headers: {
-            //            "Content-Type": "application/json"
-            //        },
-            //        data: event
-            //    }).then(function (response) {
-            //        if (response && response.data) {
-            //            event = convertToCalendarEvent(response.data)
-            //            storedEvents.push(event);
-            //            return storedEvents;
-            //        }
-            //    });
-            //    return addEventRequest;
-            //}
-
-            //function deleteEvent(event) {
-            //    var deleteEventRequest = $http({
-            //        method: "DELETE",
-            //        url: apiUri + 'event?eventGuid=' + event.guid,
-            //        dataType: 'json',
-            //        headers: {
-            //            "Content-Type": "application/json"
-            //        },
-            //        data: event
-            //    }).then(function (response) {
-            //        if (response && response.data) {
-            //            storedEvents = _.reject(storedEvents, function (item) { return item.guid == event.guid; });
-            //        }
-            //        return storedEvents;
-            //    });
-            //    return deleteEventRequest;
-            //}
-
-
             return {
                 GetMovies: getMovies,
-                //AddNewEventEntry: addNewEvent,
-                //DeleteEventEntry: deleteEvent,
-                //EditEvent: editEvent
             };
         }]);
 })();
